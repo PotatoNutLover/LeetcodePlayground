9969770a3258051129f89e917e41ff98a5775dcf
potatoNut <potatoNut@DESKTOP-881NLOD>
2023-09-02 00:04:43 +0300

    Добавьте файлы проекта.

---
+++ LeetcodePlayground/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetcodePlayground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new CountingBits().CountBits(5);
+
+            foreach (int i in mas)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
